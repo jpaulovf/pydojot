29e7d1e18900722e0ef6a111b96d920772cda531
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2022-05-18 18:28:21 -0300

    Apply yapf to reflow code, acording to PEP-8

--- pydojot/exceptions.py
+++ pydojot/exceptions.py
@@ -3,6 +3,8 @@ Custom exceptions
 
 """
 
+
 class RequestException(Exception):
+
     def __init__(self, status_code, body):
         super().__init__(f"Status code = {status_code}, Body = {body}")
--- test_pydojot_existing.py
+++ test_pydojot_existing.py
@@ -7,7 +7,9 @@ from pydojot.session import DojotSession
 import random
 
 # Open a new session
-session = DojotSession("http://localhost:8000", username="admin", password="admin")
+session = DojotSession("http://localhost:8000",
+                       username="admin",
+                       password="admin")
 
 # Loading existing templates in the opened session
 existing_templates = session.load_templates()
@@ -24,10 +26,11 @@ for device in existing_devices:
     print(device)
 
 # Sending data to existing devices
-for k in range(0,100):
+for k in range(0, 100):
     for device in existing_devices:
-        device.set_attrs({"MyString": f"test {k}",
-                          "MyInteger": random.randint(0,10),
-                          "MyFloat": random.random()})
+        device.set_attrs({
+            "MyString": f"test {k}",
+            "MyInteger": random.randint(0, 10),
+            "MyFloat": random.random()
+        })
         session.publish_attrs(device)
-
